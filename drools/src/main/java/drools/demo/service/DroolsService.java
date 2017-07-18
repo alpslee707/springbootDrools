1a70b787c4771dd4e9171e7a2cdd590c91cefb25
alpslee707 <leetr.nature@hotmail.com>
2017-07-18 18:53:12 +0800

    Create DroolsService.java

---
+++ drools/src/main/java/drools/demo/service/DroolsService.java
@@ -0,0 +1,31 @@
+package drools.demo.service;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.springframework.stereotype.Service;
+
+import drools.demo.Model.Message;
+
+
+@Service
+public class DroolsService {
+	public static String fireRule() {
+		// load up the knowledge base
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieSession kSession = kContainer.newKieSession("ksession-rules");
+		// go !
+		Message message = new Message();
+		message.setMessage("Hello World");
+		message.setStatus(Message.GOODBYE);
+		kSession.insert(message);// 插入
+		kSession.fireAllRules();// 执行规则
+		kSession.dispose();
+		return message.getMessage();
+	}
+	
+	public static void main(String[] args) {
+        fireRule();
+    }
+}
